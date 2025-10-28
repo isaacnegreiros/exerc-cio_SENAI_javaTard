3536773c96298e231899b0c048515ec0068d2067
isaacnegreiros <britosmendes11@gmail.com>
2025-10-28 16:17:09 -0300

    Add files via upload

--- atividade17/atv_1.java
+++ atividade17/atv_1.java
@@ -1,12 +1,28 @@
-package atividade17;
+package Atv_17;
 
-import java.util.Locale;
 import java.util.Scanner;
 
 public class atv_1 {
     public static void main(String[] args) {
-        Locale.setDefault(Locale.US);
-        Scanner sc= new Scanner(System.in);
-        
+        String direction;
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Para qual direção deseja seguir? (frente, direita, esquerda).");
+        direction = sc.nextLine();
+        String diracao=max(direction);
+        resultado(direction);}
+        public static String max( String direction) {
+            String resp;
+
+            if (direction.equalsIgnoreCase("Frente")) {
+                resp = "Movendo para frente.";
+            } else if (direction.equalsIgnoreCase("direita")) {
+                resp = "Movendo para a direita.";
+            } else {
+                resp = "Movendo para a esquerda.";
+            }return resp;
+        }public static void resultado(String resp){
+        System.out.println(resp);
     }
 }
+
+
